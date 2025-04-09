c9e628c55e39f9c95eba4bdc4837fb8f0b070d0c
Jessie366 <jessieyin366@gmail.com>
2025-04-09 23:03:19 +1000

    modify eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Add the custom rule to disable no-unused-vars
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",  // Disable the rule here
+    },
+  },
 ];
 
 export default eslintConfig;
