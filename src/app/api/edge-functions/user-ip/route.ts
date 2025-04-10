f5a0973e8faa2a5eea1a7ef17df3f6d6f57ee098
Jessie366 <jessieyin366@gmail.com>
2025-04-10 10:41:18 +1000

    error

--- src/app/api/edge-functions/user-ip/route.ts
+++ src/app/api/edge-functions/user-ip/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export const config = {
     runtime : "edge",
 };
 
 // GET request handler to retrieve user's IP address
-export async function GET(request : NextResponse) {
+export async function GET(request : NextRequest) {
   // Retrieve the user's IP address from the headers (This is common for serverless functions)
   const userIp = request.headers.get('x-real-ip') || request.headers.get('x-forwarded-for') || 'Unknown IP';
 
