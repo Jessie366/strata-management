5f911885ce15b1cc4eb12940e19644021a10d6df
Jessie366 <jessieyin366@gmail.com>
2025-04-10 15:42:36 +1000

    force redeploy with ip page

--- src/app/api/edge-functions/user-ip/route.ts
+++ src/app/api/edge-functions/user-ip/route.ts
@@ -8,7 +8,10 @@ export const config = {
 // GET request handler to retrieve user's IP address
 export async function GET(request : NextRequest) {
   // Retrieve the user's IP address from the headers (This is common for serverless functions)
-  const userIp = request.headers.get('x-real-ip') || request.headers.get('x-forwarded-for') || 'Unknown IP';
+  const userIp = 
+  request.headers.get('x-real-ip') || 
+  request.headers.get('x-forwarded-for') || 
+  'Unknown IP';
 
   // Return the user's IP address as a JSON response
   return NextResponse.json({ ip: userIp });
